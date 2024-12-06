f64dcd90df4216db6e3880375d308fdc1a377b48
Eranga2002 <erangamadhushann2002@gmail.com>
2024-12-06 14:07:47 +0530

    add PotentialGene.java file

---
+++ PotentialGene.java
@@ -0,0 +1,36 @@
+public class PotentialGene {
+    public static boolean isPotentialGene(String dna) {
+        //Length is a multiple of 3
+        if(dna.length() % 3 != 0) {
+            return false;
+        }
+
+        //Starts with start codon.
+
+        if(!dna.endsWith("ATG")) return false;
+
+        //Ends with a stop codon.
+
+        if(dna.endsWith("TAA")) return true;
+        if(dna.endsWith("TAG")) return true;
+        if(dna.endsWith("TGA")) return true;
+
+        // No intervening stop codons.
+
+        for(int i = 3; i < dna.length()-3; i++) {
+            if(i % 3 == 0) {
+                String codon = dna.substring(i, i + 3);
+                if(codon.equals("TAA")) return false;
+                if(codon.equals("TAG")) return false;
+                if(codon.equals("TGA")) return false;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String dna = args[0];
+        System.out.println(isPotentialGene(dna));
+    }
+}
