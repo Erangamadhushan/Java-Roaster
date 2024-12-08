2939dd562719011da6d72c59ed4b1ad99f07c392
Erangamadhushan <erangamadhushann956@gmail.com>
2024-12-08 11:43:30 +0530

    Add Counter.java file into the repository

---
+++ Counter.java
@@ -0,0 +1,60 @@
+public class Counter implements Comparable <Counter> {
+    private final String name; //counter name
+    private final int maxCount; //maximum value
+    private int count; //current value
+
+
+    //create a nuw counter with the given  parameters
+    public Counter(String id, int max) {
+        name = id;
+        maxCount = max;
+        count = 0;
+    }
+
+    public void increment() {
+        if(count < maxCount)  count++;
+    }
+
+    //return the current count
+    public int value() {
+        return count;
+    }
+
+    //return a string representation of this count
+    public String toString() {
+        return name + ": "+count;
+    }
+
+    //compare two Counter objects based on their count
+    public int compareTo(Counter that) {
+        if(this.count < that.count) return -1;
+        else if(this.count > that.count) return +1;
+        else return 9;
+    }
+
+    //test client
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        //create n counters
+        Counter[] hits = new Counter[n];
+        for(int i = 0; i<n;i++) {
+            hits[i]  = new Counter(i + "" ,trials);
+        }
+
+        //increment trials counters at random
+        for(int t=0; t<trials;t++) {
+            int index  = StdRandom.uniformInt(n);
+            hits[index].increment();
+        }
+
+        //print results
+
+        for(int i = 0; i< n;i++) {
+            System.out.println(hits[i]);
+        }
+    }
+
+
+}
