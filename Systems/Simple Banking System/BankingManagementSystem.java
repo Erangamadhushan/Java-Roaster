05eee92477240de4a0c40c7a399f168c8d109544
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-14 03:09:12 +0530

    Add core banking classes for account management functionality

--- Systems/Simple Banking System/BankingManagementSystem.java
+++ Systems/Simple Banking System/BankingManagementSystem.java
@@ -1,31 +1,45 @@
 import java.util.Scanner;
-class Bank {
-    private int accountCounter;
-    private int[] bankAcconts;
-    private double[] bankBalances;
-    private final int numAcconts = 10;
 
-    private int accountNumber;
-    private double accountBalance;
-    private String accountHolderName;
-    private String accountType;
+//declare global variables
+class GlobalVariable {
+    public static int accountCounter = 0;
+}
+class Variables {
+    //Default Data Structure in our Banking System
+    public int[] bankAcconts;
+    public double[] bankBalances;
+    public int[] bankPin;
+    public String[] accountType;
+    public final int numAcconts = 10;
+
+    //This variables are used for validate userRegisteration and relavent process
+    public String[] userIDCardDetails;
+    public String[] userNameDetails;
+
     
+}
+
+class Bank extends Variables{
     public Bank() {
         bankAcconts = new int[numAcconts];
         bankBalances = new double[numAcconts];
-        accountCounter = 0;
+        userIDCardDetails = new String[numAcconts];
+        userNameDetails = new String[numAcconts];
+        accountType = new String[numAcconts];
+        bankPin = new int[numAcconts];
     }
 
-    public void createAccount(int accountNumber, double accountBalance, String accountHolderName) {
-        this.accountNumber = accountNumber;
-        this.accountBalance = accountBalance;
-        this.accountHolderName = accountHolderName;
-    }
+}
 
-    public void deposit(double amount) {}
+//Controller Class defined for control the process of the system.
+class ProcessControler extends Bank{
+    public void deposit(double amount, int i) {
+        bankBalances[i] += amount;
+        System.out.println("Deposit Successful");
+    }
     public void withdraw(double amount) {}
     public void displayAccountDetails() {}
-
+    Scanner scanner = new Scanner(System.in);
 
     public void displayMainDashBoard() {
         System.out.println("========== Welcome to the Bank Dashboard ==========");
@@ -38,11 +52,7 @@ class Bank {
         System.out.println("------------------------------------------------------");
 
     }
-}
 
-//Controller Class defined for control the process of the system.
-class ProcessControler {
-    Scanner scanner = new Scanner(System.in);
     public int initialProcessHandling() {
         System.out.println("Enter Process Number :");
         int processNumber = scanner.nextInt();
@@ -63,13 +73,78 @@ class ProcessControler {
         scanner.nextLine();
         return processNumber;
     }
+
+    public void userRegisterationProcess() {
+        System.out.println("Enter Your ID Card Number here :");
+        String idCardNumber = scanner.nextLine();
+        System.out.println("Enter Your Name here :");
+        String accountHolderName = scanner.nextLine();
+        for(int i = 0; i <= GlobalVariable.accountCounter;i++) {
+            if(userIDCardDetails[i].equalsIgnoreCase(idCardNumber) && userNameDetails[i].equalsIgnoreCase(accountHolderName)) {
+                System.out.println("Account Already Exist");
+                return;
+            }
+        }
+        System.out.println("Enter Your PIN Number here :");
+        int pinNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter Your Account Type :");
+        String accountTypeName = scanner.nextLine();
+        System.out.println("Enter Your Initial Deposit here :");
+        double initialDeposit = scanner.nextDouble();
+        scanner.nextLine();
+
+        userIDCardDetails[GlobalVariable.accountCounter] = idCardNumber;
+        userNameDetails[GlobalVariable.accountCounter] = accountHolderName;
+        bankPin[GlobalVariable.accountCounter] = pinNumber;
+        accountType[GlobalVariable.accountCounter] = accountTypeName;
+        bankBalances[GlobalVariable.accountCounter] = initialDeposit;
+        GlobalVariable.accountCounter++;
+        System.out.println("Account Created Successfully");
+
+    }
+
+    public void depositMoneyProcess() {
+        System.out.println("Enter Your ID Card Number here :");
+        String idCardNumber = scanner.nextLine();
+        System.out.println("Enter Your Name here :");
+        String accountHolderName = scanner.nextLine();
+        for(int i = 0; i <= GlobalVariable.accountCounter;i++) {
+            if(userIDCardDetails[i].equalsIgnoreCase(idCardNumber) && userNameDetails[i].equalsIgnoreCase(accountHolderName)) {
+                System.out.println("Enter Deposit Value here :");
+                double depositValue = scanner.nextDouble();
+                scanner.nextLine();
+                deposit(depositValue, i);
+            }
+        }
+    }
+    public void withdrawMoneyProcess() {
+
+    }
+
+    public void displayAccountDetailsProcess() {
+        System.out.println("Enter Your ID Card Number here :");
+        String idCardNumber = scanner.nextLine();
+        System.out.println("Enter Your Name here :");
+        String accountHolderName = scanner.nextLine();
+        System.out.println("Enter Your PIN Number here :");
+        int pinNumber = scanner.nextInt();
+        scanner.nextLine();
+        for(int i = 0; i <= GlobalVariable.accountCounter;i++) {
+            if(userIDCardDetails[i].equalsIgnoreCase(idCardNumber) && userNameDetails[i].equalsIgnoreCase(accountHolderName) && bankPin[i] == pinNumber ) {
+                System.out.println("Account Holder Name : " + userNameDetails[i]);
+                System.out.println("Account ID Card Number : " + userIDCardDetails[i]);
+                System.out.println("Account PIN Number : " + bankPin[i]);
+                
+                
+            }
+        }
+    }
 }
 
 //Call the BankingManagementSystem
 public class BankingManagementSystem {
     public static void main(String[] args) {
-        //Create Bank Object
-        Bank bankSystem = new Bank();
         ProcessControler systemProcessControler = new ProcessControler();
         boolean runningSystem = true;
 
@@ -82,8 +157,25 @@ public class BankingManagementSystem {
             int initialProcessID = systemProcessControler.initialProcessHandling();
             switch (initialProcessID) {
                 case 1:
-                    bankSystem.displayMainDashBoard();
+                    systemProcessControler.displayMainDashBoard();
                     int runningProcessID = systemProcessControler.runningProcessHandling();
+                    switch(runningProcessID) {
+                        case 1:
+                            systemProcessControler.userRegisterationProcess();
+                        break;
+                        case 2:
+                            systemProcessControler.depositMoneyProcess();
+                        break;
+                        case 3:
+                            systemProcessControler.withdrawMoneyProcess();
+                        break;
+                        case 4:
+                            systemProcessControler.displayAccountDetailsProcess();
+                        break;
+                        default:
+                            System.out.println("Invalid Input !!");
+                        
+                    }
                 break;
                 case 2:
                     System.out.println("Do you want to exit the system?");
